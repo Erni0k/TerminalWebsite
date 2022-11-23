dd738df5e07070aa8871bf0e528bb456118a7617
Erni <lezu4500@gmail.com>
2022-11-23 09:54:32 +0100

    init

---
+++ commands.js
@@ -0,0 +1,45 @@
+var twitter = "https://twitter.com/akaErni__";
+var github = "https://github.com/Erni0k";
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/akaErni__' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/Erni0k' + "</a>",
+  'discord:       <a>Erni#0666</a>',
+  "<br>"
+];
+help = [
+  "<br>",
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display my socials',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">pokemon</span>        What Pokemon are you today?',
+  '<span class="command">date</span>           Displays current date',
+  "<br>",
+];
+
+banner = [
+'           __________                                 ',
+'         .\'----------`.                              ',
+'         | .--------. |                             ',
+'         | |########| |       __________              ',
+'         | |########| |      /__________\\             ',
+'.--------| `--------\' |------|    --=-- |-------------.\n',
+'|        `----,-.-----\'      |o ======  |             | _____               _  ',
+'|       ______|_|_______     |__________|             ||  ___|             (_) ',
+'|      /  %%%%%%%%%%%%  \\                             || |__   _ __  _ __   _  ',
+'|     /  %%%%%%%%%%%%%%  \\                            ||  __| | \'__|| \'_ \\ | | ',
+'|     ^^^^^^^^^^^^^^^^^^^^                            || |___ | |   | | | || | ',
+'+-----------------------------------------------------+\\____/ |_|   |_| |_||_|',
+'^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ',
+  '<span class="color2">Welcome to my Web Terminal</span>',
+  "<span class=\"color2\">For a list of available commands, use </span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
---
+++ main.js
@@ -0,0 +1,161 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let currentDate = `${day}-${month}-${year}`;
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+var randomPok = getRandomInt(1154);
+
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("guest@Erni.tk~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "whoami":
+      loopLines(whoami, "color2 margin", 80);
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    case "twitter":
+      addLine("Opening Twitter...", "color2", 0);
+      newTab(twitter);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    case "pokemon":
+      fetch('https://pokeapi.co/api/v2/pokemon/'+randomPok+'/')
+          .then(res=>{
+            if(res.ok){
+              res.json()
+                  .then(data => {
+                    var pokemonName=data.name;
+                    addLine('<br>')
+                    addLine(pokemonName,"color2")
+                    addLine('More info at:',"color2")
+                    addLine('https://bulbapedia.bulbagarden.net/wiki/'+pokemonName,"color2")
+                    addLine('<br>')
+                  })
+            }else{
+              addLine('Something went wrong try refreshing the page ',"color2")
+            }
+          })
+      break;
+    case "date":
+      addLine('<br>')
+      addLine('Today is:',"color2")
+      addLine(currentDate,"color2")
+      addLine('<br>')
+      break;
+    default:
+      addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
+
